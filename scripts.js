3d288f506655188661a494d2bbba01e294a157e0
klanza <klanza91@gmail.com>
2018-08-01 00:17:15 -0700

    Created return time display

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
   setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     // check to stop
@@ -25,4 +28,14 @@ function displayTimeLeft(seconds) {
   }${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Return at ${adjustedHour}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}`;
 }
